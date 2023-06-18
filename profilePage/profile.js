aa46c9867086fa7b5984cca3c1ec30648d098c89
Keshav Monga <mongaanmol40@gmail.com>
2023-06-19 01:51:02 +0530

    added dp change functionality

---
+++ profilePage/profile.js
@@ -0,0 +1,17 @@
+const div = document.getElementsByClassName('dp')[0]
+const input = document.getElementsByTagName('input')[0]
+const dp = document.getElementsByTagName('img')[1]
+
+console.log(dp, div, input)
+
+input.addEventListener('change', (e) => {
+  let file = e.target.files[0]
+  const reader = new FileReader();
+  reader.addEventListener('load', (e) => {
+    dp.src = e.target.result
+  })
+  reader.readAsDataURL(file);
+
+})
+
+dp.addEventListener('click', () => { input.click() })
