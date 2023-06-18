737bd2266bbc7ee3b0e2097ef6fb8be05b3bb5e1
Keshav Monga <mongaanmol40@gmail.com>
2023-06-19 01:28:25 +0530

    added login form validations using jquery

--- chatPage/chat.js
+++ chatPage/chat.js
@@ -1,7 +1,28 @@
 const chatBoxInput = document.querySelector('#chatBox')
 const sendButton = document.querySelector('.fa-paper-plane')
 const messages = document.querySelector('.messages')
+const userContainer = document.querySelector('.users-container')
 const regex = new RegExp('^(?=\s*$)')
+
+const users = [
+  { name: "Hinish", lastMsg: 'Hi' },
+  { name: "Hridyesh", lastMsg: 'Bro' },
+  { name: "Ishmeet", lastMsg: 'Hello' },
+  { name: "Jatin", lastMsg: 'Bhai' },
+]
+
+users.forEach(user => {
+  userContainer.innerHTML +=
+    `<div class="user">
+    <div class="user-avatar"></div>
+    <div class="user-info">
+      <span>${user.name}</span>
+      <p>${user.lastMsg}</p>
+    </div>
+  </div>
+  <div class="divider"></div>`;
+})
+
 
 messages.scrollTo({ top: '100%' })
 
@@ -12,7 +33,7 @@ const sendMessage = (e) => {
   const div = document.createElement('div')
   const span = document.createElement('span')
   const recieve = (msg.includes(':recieve') && msg.indexOf(':') === 0)
-  recieve ? null :span.classList.add("sent");
+  recieve ? null : span.classList.add("sent");
   msg = recieve ? msg.substr(9) : msg
   span.innerHTML = msg
   div.appendChild(span)
